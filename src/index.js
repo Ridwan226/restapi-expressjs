b20c67b181583577ed948f050698911dcfd6ee06
Ridwan Ganteng <ridwan.endan26@gmail.com>
2023-09-02 03:14:19 +0700

    Setup Middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const usersRoutes = require('./routes/users');
 const UserController = require('./controller/users');
+const MiddlewareLogRequest = require('./middleware/logs');
 const app = express();
 
-app.use('/users', usersRoutes);
+app.use(MiddlewareLogRequest.logRequest);
 
-app.get('/', UserController.getAllUers);
-app.post('/', UserController.createNewUers);
+app.use('/users', usersRoutes);
 
 app.listen(4000, () => {
   console.log('Redy server Port 4000');
